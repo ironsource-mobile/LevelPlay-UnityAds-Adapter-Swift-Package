6ebafe26802f029c561d7aa89ec949f06c4570cf
Gal Yedidovich <gal.yedidovich@unity3d.com>
2026-01-21 12:34:52 +0200

    Initial release v5.4.0
    - UnityAds adapter v5.4.0
    - Network SDK v4.16.5
    - Min iOS 13.0

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+  name: "LevelPlay-UnityAds-Adapter-Swift-Package",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(name: "UnityAdsAdapter", targets: ["UnityAdsAdapter"]),
+  ],
+  dependencies: [
+    // TODO: Add UnityAds SPM package when available
+    // .package(url: "<UnityAds-SPM-URL>", exact: "4.16.5"),
+    .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.0.0"..<"10.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "UnityAdsAdapter",
+      dependencies: [
+        "UnityAdsAdapterSDK",
+        // TODO: Add UnityAds product when SPM package is available
+        // .product(name: "<UnityAdsProductName>", package: "<UnityAdsPackageName>"),
+        .product(name: "UnityMediationSDK", package: "Unity-Mediation-iAds-Swift-Package"),
+      ]
+    ),
+    .binaryTarget(
+      name: "UnityAdsAdapterSDK",
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/unityads-adapter/5.4.0/ISUnityAdsAdapter5.4.0.zip",
+      checksum: "7e63a5b080dfd2674fe5a394931f447f337fcee31afc78faabea71516f91b117"
+    )
+  ]
+)
---
+++ README.md
@@ -0,0 +1,38 @@
+# LevelPlay UnityAds Adapter Swift Package
+
+This Swift Package provides the LevelPlay UnityAds adapter for iOS.
+
+## Requirements
+
+- iOS 13.0+
+- Swift 5.0+
+- Xcode 15.0+
+
+## Installation
+
+### Swift Package Manager
+
+Add this package to your project using Xcode:
+
+1. File > Add Package Dependencies
+2. Enter the repository URL: `https://github.com/ironsource-mobile/LevelPlay-UnityAds-Adapter-Swift-Package`
+3. Select the version you want to use
+
+Or add it to your `Package.swift`:
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/ironsource-mobile/LevelPlay-UnityAds-Adapter-Swift-Package", from: "5.4.0")
+]
+```
+
+## Dependencies
+
+This package includes:
+- LevelPlay iOS SDK (Unity Mediation SDK)
+- UnityAds SDK v4.16.5
+
+## Documentation
+
+For integration guides and API documentation, visit:
+- [LevelPlay Documentation](https://developers.is.com/ironsource-mobile/ios/ios-sdk/)
