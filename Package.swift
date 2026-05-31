84abafea2f883efd07af724343bca7aa3a432036
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-05-31 08:49:32 +0000

    Release 5.8.0 - SDK 4.18.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "UnityAdsAdapter", targets: ["UnityAdsAdapter"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Unity-Technologies/Unity-Ads-Swift-Package", exact: "4.18.0"),
+    .package(url: "https://github.com/Unity-Technologies/Unity-Ads-Swift-Package", exact: "4.18.1"),
     .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.0.0"..<"10.0.0"),
   ],
   targets: [
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "UnityAdsAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/unityads-adapter/5.7.0/ISUnityAdsAdapter5.7.0.zip",
-      checksum: "830d5cd95ce4596408ed7d700f24008a1d6935bb01cf538d88b13f4bac3e535b"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/unityads-adapter/5.8.0/ISUnityAdsAdapter5.8.0.zip",
+      checksum: "ca6ba498803a76a0054f84eb5018c92f556ddf474998b387974755634f6b0cdd"
     )
   ]
 )
